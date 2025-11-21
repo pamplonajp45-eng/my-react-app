1fd44d367bd1933c8b7e4d9fd4028d5e2069c540
JPDEV <pamplonajp.45@gmail.com>
2025-11-21 16:43:03 +0800

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/my-react-app/sw.js', { scope: '/my-react-app/' })})}
